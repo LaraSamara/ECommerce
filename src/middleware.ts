1c4eff89a7083ae1ab663c298ec6b397024b4a47
LaraSamara <larasamara2002@gmail.com>
2025-09-24 00:55:54 +0300

    Fix Token

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,7 @@
 import { NextRequest, NextResponse } from "next/server.js";
 
 export function middleware(request: NextRequest) {
-    const cookieName =  process.env.NODE_ENV === "production"
-        ? "__Secure-next-auth.session-token"
-        : "next-auth.session-token";
+    const cookieName = process.env.NEXTAUTH_COOKIE_NAME || '';
     const token = request.cookies.get(cookieName)?.value;
     if (!token) {
         return NextResponse.redirect(new URL('/signin', request.url));
