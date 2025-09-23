10bfbec4dddd9720f9e375157c38ac665c19c6a4
LaraSamara <larasamara2002@gmail.com>
2025-09-24 00:46:43 +0300

    Fix Token Name

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -1,8 +1,11 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers.js"
 
-export const getToken = async() => {
-    const encodedToken = (await cookies()).get("next-auth.session-token")?.value;
-    const decodedToken = await decode({token: encodedToken, secret: process.env.AUTH_SECRET!});
+export const getToken = async () => {
+    const cookieName = process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token";
+    const encodedToken = (await cookies()).get(cookieName)?.value;
+    const decodedToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! });
     return decodedToken?.token;
 }
