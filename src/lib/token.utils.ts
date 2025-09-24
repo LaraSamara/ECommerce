53176a00d6ddd98ea86ab3108d3199e3af065d6d
LaraSamara <larasamara2002@gmail.com>
2025-09-24 22:12:14 +0300

    Fix Cookie Name

--- src/lib/token.utils.ts
+++ src/lib/token.utils.ts
@@ -2,7 +2,7 @@ import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers.js"
 
 export const getToken = async () => {
-    const cookieName = process.env.NEXTAUTH_COOKIE_NAME || '';
+    const cookieName = process.env.NEXTAUTH_COOKIE_NAME!;
     const encodedToken = (await cookies()).get(cookieName)?.value;
     const decodedToken = await decode({ token: encodedToken, secret: process.env.AUTH_SECRET! });
     return decodedToken?.token;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server.js";
 
 export function middleware(request: NextRequest) {
-    const cookieName = process.env.NEXTAUTH_COOKIE_NAME || '';
+    const cookieName = process.env.NEXTAUTH_COOKIE_NAME!;
     const token = request.cookies.get(cookieName)?.value;
     if (!token) {
         return NextResponse.redirect(new URL('/signin', request.url));
